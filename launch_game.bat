44612b21a403a3eaecf0e68e898accd348e1b2be
Eric <notericsoft@gmail.com>
2018-04-29 00:24:38 -0500

    Added batch files
    For use outside of an IDE, batch files are now included to start the server

---
+++ launch_game.bat
@@ -0,0 +1,5 @@
+@echo off
+title OrionAlpha - Game
+set CLASSPATH=.;bin\target\OrionAlpha.jar
+java -Xmx200m -Dwzpath=wz/ -Dio.netty.leakDetection.level=advanced -XX:NewRatio=50 -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:+HeapDumpOnOutOfMemoryError -XX:-DisableExplicitGC -XX:+AggressiveOpts -Xnoclassgc -XX:+UseNUMA -XX:+UseFastAccessorMethods -XX:ReservedCodeCacheSize=48m -XX:MaxGCPauseMillis=300 -XX:GCPauseIntervalMillis=400 -XX:+UseTLAB -XX:+AlwaysPreTouch game.GameApp
+pause

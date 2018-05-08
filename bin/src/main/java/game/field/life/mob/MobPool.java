f01a003d681688666ff5b7d189efb874a0408dbf
Eric <notericsoft@gmail.com>
2018-05-07 21:18:33 -0500

    Implemented MobPool
    smh forgot to include this in previous commit

--- bin/src/main/java/game/field/life/mob/MobPool.java
+++ bin/src/main/java/game/field/life/mob/MobPool.java
@@ -17,10 +17,64 @@
  */
 package game.field.life.mob;
 
+import game.field.MovePath;
+import network.packet.LoopbackPacket;
+import network.packet.OutPacket;
+
 /**
  *
  * @author Eric
  */
 public class MobPool {
     
+    public static OutPacket onMobEnterField(Mob mob) {
+        OutPacket packet = new OutPacket(LoopbackPacket.MobEnterField);
+        mob.encodeInitData(packet);
+        return packet;
+    }
+    
+    public static OutPacket onMobLeaveField(int mobID, byte deadType) {
+        OutPacket packet = new OutPacket(LoopbackPacket.MobLeaveField);
+        packet.encodeInt(mobID);
+        packet.encodeByte(deadType);
+        return packet;
+    }
+    
+    public static OutPacket onAffected(int mobID, int skillID, short delay) {
+        OutPacket packet = new OutPacket(LoopbackPacket.MobAffected);
+        packet.encodeInt(mobID);
+        packet.encodeInt(skillID);
+        packet.encodeShort(delay);
+        return packet;
+    }
+    
+    public static OutPacket onCtrlAck(int mobID, short mobCtrlSN, boolean nextAttackPossible, short mp) {
+        OutPacket packet = new OutPacket(LoopbackPacket.MobCtrlAck);
+        packet.encodeInt(mobID);
+        packet.encodeShort(mobCtrlSN);
+        packet.encodeBool(nextAttackPossible);
+        packet.encodeShort(mp);
+        return packet;
+    }
+    
+    public static OutPacket onMobChangeController(Mob mob, int mobID, byte level) {
+        OutPacket packet = new OutPacket(LoopbackPacket.MobChangeController);
+        packet.encodeByte(level);
+        if (level != 0) {
+            mob.encodeInitData(packet);
+        } else {
+            packet.encodeInt(mobID);
+        }
+        return packet;
+    }
+    
+    public static OutPacket onMove(int mobID, boolean nextAttackPossible, byte left, int skillID, MovePath mp) {
+        OutPacket packet = new OutPacket(LoopbackPacket.MobMove);
+        packet.encodeInt(mobID);
+        packet.encodeBool(nextAttackPossible);
+        packet.encodeByte(left);
+        packet.encodeInt(skillID);
+        mp.encode(packet);
+        return packet;
+    }
 }
